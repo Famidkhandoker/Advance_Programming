3284d636c586493cb38543e67f93e2cb281443a9
FAMID KHANDOKER <famidkhandoker@gmail.com>
2026-01-25 23:10:44 +0600

    Add files via upload

---
+++ lab4/Test2.java
@@ -0,0 +1,13 @@
+public class Test2 {
+            public static void main(String[] args) {
+
+            PartTimeEmployee p1 = new PartTimeEmployee("Rahim", 22, "Dhaka", "IT", "Intern", 40, 300);
+            System.out.println(p1);
+
+            FullTimeEmployee p2 = new FullTimeEmployee("Karim", 30, "Chittagong", "HR", "Manager", 15000, 25);
+            System.out.println(p2);
+
+
+
+    }
+}
