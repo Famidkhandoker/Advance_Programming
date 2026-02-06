aa87504e0037c24b479d33b5cff51cdd8561f597
FAMID KHANDOKER <famidkhandoker@gmail.com>
2026-02-06 18:52:26 +0600

    Add files via upload

---
+++ lab4/Practise_inheritance/Test4.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Test4 {
+    static void main() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter side1: ");
+        double side1 = input.nextDouble();
+
+        System.out.print("Enter side2: ");
+        double side2 = input.nextDouble();
+
+        System.out.print("Enter side3: ");
+        double side3 = input.nextDouble();
+
+        System.out.print("Enter color: ");
+        String color = input.next();
+
+        System.out.print("Is the triangle filled? (true/false): ");
+        boolean filled = input.nextBoolean();
+
+        Triangle triangle = new Triangle(side1, side2, side3);
+        triangle.setColor(color);
+        triangle.setFilled(filled);
+
+        System.out.println("\n" + triangle.toString());
+        System.out.println("Area: " + triangle.getArea());
+        System.out.println("Perimeter: " + triangle.getPerimeter());
+        System.out.println("Color: " + triangle.getColor());
+        System.out.println("Filled: " + triangle.isFilled());
+    }
+}
