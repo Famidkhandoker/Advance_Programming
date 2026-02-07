e3de18bb0046d2e89f5583409debb929c6f28824
FAMID KHANDOKER <famidkhandoker@gmail.com>
2026-02-07 18:32:30 +0600

    Update Main.java

--- lab4/A Payment Processing System/Main.java
+++ lab4/A Payment Processing System/Main.java
@@ -10,3 +10,7 @@ public class Main {
         service.execuA Payment Processing SystemtePayment(paymentRef);
     }
 }
+
+//output
+// Processing mobile banking payment from 017XXXXXXXX via bKash of amount: 1500.0
+
